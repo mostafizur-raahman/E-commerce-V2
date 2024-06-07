ee0407e9c941ccb6244d704bb4cf6a049e0d453e
mostafizur-raahman <mostafizurrahman@0202@gmail.com>
2024-06-07 22:58:04 +0600

    add product route in inde x

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const { RoleRoutes } = require("../../modules/Role/role.routes.js");
 const { UserRoutes } = require("../../modules/User/user.route.js");
 const { CategoryRoutes } = require("../../modules/Category/category.route.js");
+const { ProductRoutes } = require("../../modules/Product/product.route.js");
 
 const router = Router();
 
 router.use("/roles", RoleRoutes);
 router.use("/users", UserRoutes);
 router.use("/category", CategoryRoutes);
+router.use("/products", ProductRoutes);
 
 module.exports = router;
